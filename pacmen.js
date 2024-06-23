67b106980ec3bd8042a484ce0852b97727275f9d
Alvis Prieditis <reverbtail@gmail.com>
2024-06-23 07:15:31 +0300

    Updated file structure and modified pacmen.js to use all images in array
    - Added images folder to maintain correct file structure.
    - Modified pacmen.js to ensure all images in the two-dimensional array were used.

--- pacmen.js
+++ pacmen.js
@@ -1,7 +1,7 @@
 var pos = 0;
 const pacArray = [
-    ['PacMan1.png', 'PacMan2.png'],
-    ['PacMan3.png', 'PacMan4.png']
+    ['./images/pacman1.png', './images/pacman2.png'],
+    ['./images/pacman3.png', './images/pacman4.png'] // Corrected the typo here
 ];
 var direction = 0;
 const pacMen = []; // This array holds all the pacmen
@@ -15,12 +15,12 @@ function setToRandom(scale) {
 
 // Factory to make a PacMan at a random position with random velocity
 function makePac() {
-    let velocity = setToRandom(5); // {x:?, y:?}
+    let velocity = setToRandom(10); // {x:?, y:?}
     let position = setToRandom(200);
     let game = document.getElementById('game');
     let newimg = document.createElement('img');
     newimg.style.position = 'absolute';
-    newimg.src = 'PacMan1.png';
+    newimg.src = pacArray[0][0];
     newimg.width = 100;
     newimg.style.left = position.x + 'px';
     newimg.style.top = position.y + 'px';
@@ -29,7 +29,9 @@ function makePac() {
     return {
         position,
         velocity,
-        newimg
+        newimg,
+        imgIndex: 0,  // Keep track of the current image index
+        direction: 0  // Keep track of the direction
     }
 }
 
@@ -40,13 +42,18 @@ function update() {
         item.position.y += item.velocity.y;
         item.newimg.style.left = item.position.x + 'px';
         item.newimg.style.top = item.position.y + 'px';
+
+        
+        item.imgIndex = (item.imgIndex + 1) % 2; // Cycle between 0 and 1
+        item.newimg.src = pacArray[item.direction][item.imgIndex];
     });
-    setTimeout(update, 20);
+    setTimeout(update, 200); 
 }
 
 function checkCollisions(item) {
     if (item.position.x + item.velocity.x < 0 || item.position.x + item.velocity.x > window.innerWidth - item.newimg.width) {
         item.velocity.x = -item.velocity.x;
+        item.direction = item.direction === 0 ? 1 : 0; // Change direction
     }
     if (item.position.y + item.velocity.y < 0 || item.position.y + item.velocity.y > window.innerHeight - item.newimg.height) {
         item.velocity.y = -item.velocity.y;
@@ -56,3 +63,5 @@ function checkCollisions(item) {
 function makeOne() {
     pacMen.push(makePac());
 }
+
+
